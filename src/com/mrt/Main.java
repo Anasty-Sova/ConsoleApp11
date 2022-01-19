de9d34008ecea5afdd9ffc8079f78c0186f739a8
Анастасия Колесниченко <71977374+Anasty-Sova@users.noreply.github.com>
2022-01-19 10:14:37 +0300

    Initial commit

---
+++ src/com/mrt/Main.java
@@ -0,0 +1,37 @@
+package com.mrt;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("mass.txt");
+        if (file.exists())
+        {
+            System.out.println("File existed");
+        }
+        else {
+            System.out.println("File created!");
+        }
+
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String str;
+        int sum = 0;
+        int sa = 0;
+        int co=42;
+
+    while ((str =bufferedReader.readLine())!=null)
+    {
+        sum += Integer.parseInt(str);
+        sa = sum / co;
+    }
+        System.out.println(sum);
+        System.out.println(sa);
+    }
+}
